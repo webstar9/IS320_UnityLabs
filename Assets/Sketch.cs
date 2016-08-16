6b1d5d9667286460f7b582ed2d9a6d640c8c486c
webstar9 <pweb047@aucklanduni.ac.nz>
2016-08-16 12:26:55 +1200

    Added the Lab Files
    Up to the tutorial Absolutely (Pre)Fabulous!

---
+++ Assets/Sketch.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sketch : MonoBehaviour {
+
+    public GameObject myPrefab;
+
+
+    void Start() {
+
+        int totalCubes = 8;
+        int totalDistance = 5;
+
+        for (int i = 0; i < totalCubes; i++)
+        {
+            float perc = i / (float)totalCubes;
+            float x = perc * totalDistance;
+            float y = 5.0f;
+            float z = 0.0f;
+
+            var newCube = (GameObject)Instantiate(myPrefab, new Vector3(x, y, z), Quaternion.identity);
+            newCube.GetComponent<CubeScript>().SetSize(1.0f - perc);
+            newCube.GetComponent<CubeScript>().rotateSpeed = 1.0f - perc; // Random.value;
+
+    }
+
+}
+
+	void Update () {
+	
+	}
+}
